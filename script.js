b2c2129a6f55e51858753a36fb08376e13fb9d95
Jorge Santos <jorgesantos214@gmail.com>
2023-09-26 13:12:04 -0400

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+// Set the date of your next date with your girlfriend (replace with your actual date)
+const nextDate = new Date('2023-09-30T08:05:00');
+
+function updateCountdown() {
+    const now = new Date();
+    const timeDifference = nextDate - now;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('days-value').textContent = days;
+    document.getElementById('hours-value').textContent = hours;
+    document.getElementById('minutes-value').textContent = minutes;
+    document.getElementById('seconds-value').textContent = seconds;
+}
+
+setInterval(updateCountdown, 1000); // Update the countdown every second
+updateCountdown(); // Initial call to display the countdown immediately
